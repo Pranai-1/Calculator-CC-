47b03c8caf80a406f3d20b9da60adc28481c6525
Pranai-1 <reddypranai2017@gmail.com>
2023-12-19 13:00:07 +0530

    completed calculator

--- src/Factory.js
+++ src/Factory.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 function Factory(props) {
   const [showHello, setShowHello] = useState(false);
-
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowHello(true);
